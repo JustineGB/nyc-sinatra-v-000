45beb416538d6c7314c7da08424180f360cad712
Justine B <justinegayton@gmail.com>
2018-11-19 19:41:14 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -21,30 +21,28 @@ class FiguresController < ApplicationController
   post '/figures' do
   #binding.pry
     @figure = Figure.create(:name => params[:figure][:name])
-    #if !params["landmark"]["name"].include?(landmark)
-    #@figure.landmarks << Landmark.create(name: params["landmark"]["name"])
-    #end
-    #if !params["title"]["name"].empty?
-      #@figure.titles << Title.create(name: params["title"]["name"])
-    #end
-  #  if !params[:figure].keys.include?("landmark_id")
-    #  params[:figure]["landmark_id"] = []
-  #  end
-  #  @figure = Figure.find(params[:id])
-  #  @figure.update(params["figure"])
-  #  if !params["landmark"]["name"].empty?
-  #  @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
-  #  end
-    #@figure.landmarks = Landmark.find_or_create_by(:name => params[:landmarks])
-    #@figure.titles = Landmark.find_or_create_by(:name => params[:titles])
-  #  @figure.save
-  #  redirect to "figures/#{@figure.id}"
+#    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(name: params[:title][:name])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+  @figure.save
+    redirect to "figures/#{@figure.id}"
   end
+  post '/figures' do
+          @figure = Figure.create(params[:figure])
+          if !params[:title][:name].empty?
+              @figure.titles << Title.create(params[:title])
+          end
+          if !params[:landmark][:name].empty?
+              @figure.landmarks << Landmark.create(params[:landmark])
+          end
+          @figure.save
+          redirect to "/figures"
+      end
 
-    #@title = params[:title]
-    #@title_ids = params[:figure][:title_ids]
-    #@landmark = params[:landmark]
-    #@landmark_ids = params[:figure][:landmark_ids]
 
 
   get '/figures/:id/edit' do
